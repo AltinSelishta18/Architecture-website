57646a46be19f03ac053935a57546888eef42c52
Altin Selishta <AltinSelishta14@gmail.com>
2026-04-02 03:11:36 +0200

    Saving the work on services image slider

--- web-folders/JS-files/index.js
+++ web-folders/JS-files/index.js
@@ -63,6 +63,56 @@ const Scroll_Animation = new IntersectionObserver((entries) =>{
 cards.forEach(card => Scroll_Animation.observe(card));
 btnObserver.observe(features_section);
 
+// Service section image slider
+
+const small_img = document.querySelector(".services-small-img img");
+const big_img = document.querySelector(".service-big-img img");
+const progress_line = document.querySelector(".progress-line");
+
+
+const services_images = [
+  "../images/services-images/service-interior.jpg",
+  "../images/feature-images/feature-img-2.jpg",
+  "../images/services-images/service-concept.jpg",
+  "../images/services-images/service-commercial.jpg"                
+]
+
+let services_index = 0
+let width = 0;
+let limit = 90;
+
+function Show_image(){
+    big_img.src = services_images[services_index];
+
+    let nextIndex = (services_index + 1) % services_images.length;
+    small_img.src = services_images[nextIndex];
+}
+
+function Next_image(){
+  services_index = (services_index + 1) % services_images.length;
+  Show_image();
+
+  width = 0;
+  progress_line.style.width = "0%";
+}
+
+function progress(){
+    setInterval(() =>{
+      width++;
+
+      progress_line.style.width = width + "%";
+
+      if(width >= limit){
+        Next_image();
+      }
+    }, 50)
+}
+
+Show_image()
+progress();
+
+
+
 
 
 
