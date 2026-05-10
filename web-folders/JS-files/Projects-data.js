a867791c9d9afc2123cd6dc94b17b125b2a0b115
Altin Selishta <AltinSelishta14@gmail.com>
2026-05-10 03:08:26 +0200

    SAving the projects new  changes (input search added and option form deleted)

---
+++ web-folders/JS-files/Projects-data.js
@@ -0,0 +1,69 @@
+// This file will contain the projects page data
+
+export const projects = [
+  {
+    id: 1,
+    title: "Modern Interior",
+    type: "interior",
+    location: "Prishtinë",
+    year: 2024,
+    img: "/web-folder/images/projects-interior.jpg",
+    alt: "Interior architecture project",
+    content: "Modern minimalist interior architecture."
+  },
+
+  {
+    id: 2,
+    title: "Commercial Center",
+    type: "commercial",
+    location: "Gjilan",
+    year: 2023,
+    img: "/web-folders/images/projects-commercial.jpg",
+    alt: "Commercial architecture project",
+    content: "Large scale commercial building."
+  },
+
+  {
+    id: 3,
+    title: "Shopping Mall",
+    type: "commercial",
+    location: "Ferizaj",
+    year: 2022,
+    img: "/web-folders/images/shopping-mall-img.jpg",
+    alt: "Shopping mall architecture",
+    content: "Contemporary shopping mall design."
+  },
+
+  {
+    id: 4,
+    title: "Urban Planning",
+    type: "urban",
+    location: "Prizren",
+    year: 2024,
+    img: "/web-folders/images/urban-planning-img.jpg",
+    alt: "Urban planning project",
+    content: "Urban planning and landscape concept."
+  },
+
+  {
+    id: 5,
+    title: "Concept Design",
+    type: "Exterior",
+    location: "Pejë",
+    year: 2025,
+    img: "/web-folders/images/service-concept.jpg",
+    alt: "Architecture concept design",
+    content: "Creative architecture concept."
+  },
+
+  {
+    id: 6,
+    title: "Interior Space",
+    type: "interior",
+    location: "Kamenicë",
+    year: 2023,
+    img: "/web-folders/images/service-interior.jpg",
+    alt: "Luxury interior space",
+    content: "Elegant residential interior."
+  }
+];
--- web-folders/JS-files/projects.js
+++ web-folders/JS-files/projects.js
@@ -1,5 +1,7 @@
 // Projects file JavaScript code
 
+import { projects } from "./Projects-data.js";
+
 const hamburger = document.getElementById("hamburger");
 const Projects_nav = document.querySelector(".projects-nav");
 
@@ -7,3 +9,14 @@ hamburger.addEventListener("click", function(){
     hamburger.classList.toggle("open");
     Projects_nav.classList.toggle("showMenu")
 })
+
+// Here will be created the logic for the projects filtration
+
+
+
+formular.addEventListener("submit", function(e){
+    e.preventDefault();
+
+});
+
+
